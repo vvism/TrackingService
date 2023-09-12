e90bbb0cedc4b4ff8ae95eb4a819eb6b05a7ba8d
Care <Care@DESKTOP-OVMEDSD>
2023-09-12 11:53:34 +0530

    commit cicd projet

---
+++ CICD/src/com/hcl/practics/CICDPractics.java
@@ -0,0 +1,12 @@
+package com.hcl.practics;
+
+public class CICDPractics {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("CI/CD Working fine");
+		}
+	}
+}
